379fc9f417e33d0fd553a57286f7b11fa802cef4
xirurgx3d <xirurgx3d@gmail.com>
2021-10-05 21:11:41 +0300

    отключил табы в доставке

--- client/src/pages/cart/CartChoice.tsx
+++ client/src/pages/cart/CartChoice.tsx
@@ -11,14 +11,17 @@ const CartChoise: FC = () => {
     const activeChoice: keyof typeof CART_CHOICE = useSelector((state: RootState) => state.cart.cart_choice);
 
     const deliveryCN = cn("cart__choice__item", { active: activeChoice === CART_CHOICE.DELIVERY });
-    const pickupCN = cn("cart__choice__item", { active: activeChoice === CART_CHOICE.PICKUP });
-    const onspotCN = cn("cart__choice__item", { active: activeChoice === CART_CHOICE.ONSPOT });
+    const pickupCN = cn("cart__choice__item", { active: false }); //activeChoice === CART_CHOICE.PICKUP
+    const onspotCN = cn("cart__choice__item", { active: false }); // activeChoice === CART_CHOICE.ONSPOT
+
+    //dispatch(cartChoiceAction(CART_CHOICE.PICKUP))
+    //dispatch(cartChoiceAction(CART_CHOICE.ONSPOT))
 
     return (
         <div className="cart__choice">
             <div className={deliveryCN} onClick={() => dispatch(cartChoiceAction(CART_CHOICE.DELIVERY))}>Доставка</div>
-            <div className={pickupCN} onClick={() => dispatch(cartChoiceAction(CART_CHOICE.PICKUP))}>С собой</div>
-            <div className={onspotCN} onClick={() => dispatch(cartChoiceAction(CART_CHOICE.ONSPOT))}>На месте</div>
+            <div className={pickupCN} >С собой</div>
+            <div className={onspotCN} >На месте</div>
         </div>
     )
 }
