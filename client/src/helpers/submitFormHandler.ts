e77470971c74ad4038915c76d903feb01e01e5de
mxxnseat <kirill_shkarun@mail.ru>
2021-10-25 13:43:21 +0300

    submit

--- client/src/helpers/submitFormHandler.ts
+++ client/src/helpers/submitFormHandler.ts
@@ -6,8 +6,6 @@ const submitHandler = <T>(values: T, meta: any)=>{
     const {cart: {promocode,cart_choice,totalPrice}} = store.getState();
     store.dispatch(changePromoCode(''));
     store.dispatch(checkOut({...values,promocode,cart_choice,totalPrice})); //{...values,cart_choice,totalPrice}
-    
-    //meta.resetForm();
 }
 
 export default submitHandler;
