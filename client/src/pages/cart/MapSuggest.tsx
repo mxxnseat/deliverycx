54d3f6473efaaca0eee27441b0c742ba1017f930
xirurgx3d <xirurgx3d@gmail.com>
2021-10-28 18:06:38 +0300

    фикс карты

--- client/src/pages/cart/MapSuggest.tsx
+++ client/src/pages/cart/MapSuggest.tsx
@@ -5,17 +5,18 @@ import { RootState } from "../../store";
 
 declare var ymaps: any;
 
-const MapSuggestComponent = ({formik,handl}: any) => {
+const MapSuggestComponent = ({formik,handl,value}: any) => {
   const { name } = useSelector((state: RootState) => state.address.address.city);
   
-  const geoCode = (request: string) => {
+  const geoCode = (request: string,set:boolean) => {
     return ymaps.geocode(request)
       .then((res: any) => {
         const getObj = res.geoObjects.get(0);
         const validAdress: string = getObj?.properties.get('metaDataProperty.GeocoderMetaData.precision');
-        console.log(validAdress);
-        if (validAdress) {
+        
+        if (validAdress === 'exact') {
           handl([...getObj.geometry._coordinates])
+          set && formik.setFieldValue("address", request);
         }
       })
       .catch((e:unknown) => console.log(e))
@@ -25,15 +26,16 @@ const MapSuggestComponent = ({formik,handl}: any) => {
         const suggestView = new ymaps.SuggestView(
           'suggest', {
             provider: {
-              suggest: (function(request:string) {
-                // geoCode(request)
+              suggest: (function (request: string) {
+                geoCode(request, false)
                
                 return ymaps.suggest(name + ", " + request)
                 })
               }}
             )
             suggestView.events.add("select", function (e: any) {
-              geoCode(e.get('item').value)
+              geoCode(e.get('item').value,true)
+              
             })
             
         
@@ -44,11 +46,13 @@ const MapSuggestComponent = ({formik,handl}: any) => {
       return formikHandle(e)
     },[state])
     */
+  console.log(value)
     
     return <input 
       className="mapsPopup__input"
       type="text" id="suggest"    
       name="address"
+      
       placeholder="Введите адресс доставки"
       onClick={()=> formik.setFieldValue("address", '')}
     />;
